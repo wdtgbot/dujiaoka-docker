22824e26faadbc58c0e692c393cf7c6090c97dac
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2021-03-10 15:32:09 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@
 if [ -f "/www/wwwroot/dujiaoka/.env" ];then
     echo "配置文件已存在，请确保配置文件正确！"
     chmod -R 755 /www/wwwroot/dujiaoka
-    cd /www/wwwroot/dujiaoka && php artisan dujiao install || composer update laravel/framework && php artisan dujiao install
+    cd /www/wwwroot/dujiaoka && composer install || composer update laravel/framework && composer install
+    php artisan dujiao install
     /entrypoint supervisord
     else
     mv /www/wwwroot/dujiaoka/.env.example /www/wwwroot/dujiaoka/.env
